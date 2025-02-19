377de32254a41caea49305077565aa6bc37bbbc0
Matt Frutchey <mjfru201@gmail.com>
2025-02-19 16:27:02 -0500

    continue styling, add button for eventual meal prep component, add some css variables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 const Header = () => {
-  return (
-    <h1>Shoku<span className="shoku">食</span>GO</h1>
-  )
-}
+	return (
+		<div className="header-container">
+			<h1>
+				Shoku<span className="logo">食</span>GO
+			</h1>
+			<div className="header-buttons">
+				<button>Grocery List</button>
+				<button>Meal Planner</button>
+			</div>
+		</div>
+	);
+};
 
 export default Header;
